50a55b4b1234f5a0d225b7c7c767778ba25b1f24
Henrique <henriqueaureliocsilva@gmail.com>
2022-06-06 09:37:55 -0300

    fix: fixing image on deploy

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react'
 import { HeartIcon } from '@heroicons/react/outline'
 import { useFormik } from 'formik'
 import axios from 'axios'
+import avatar from './avatar.png'
 const MAX_TWEET_CHAR = 250;
 
 function TweetForm({ loggedInUser, onSuccess }) {
@@ -31,7 +32,7 @@ function TweetForm({ loggedInUser, onSuccess }) {
   return (
     <div className="border-b border-silver p-4 space-y-6">
       <div className="flex space-x-5">
-        <img src="./src/avatar.png" className="w-7" />
+        <img src={avatar} className="w-7" />
         <h1 className="font-bold text-xl">Pagina inicial</h1>
       </div>
       <form className='pl-12 text-lg flex flex-col' onSubmit={formik.handleSubmit}>
